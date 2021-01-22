af796cf692ccf2d641746b6ffa673bf084d57e37
yamabato <harutakutsukawa0601@gmail.com>
2021-01-22 21:38:05 +0900

    座標の記録

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var watch_id;
+var pos_list;
 
 function init() {
     watch_id = navigator.geolocation.watchPosition(write, function(e) { alert(e.message); }, {"enableHighAccuracy": true, "timeout": 20000, "maximumAge": 2000});
@@ -6,9 +7,12 @@ function init() {
 
 function write(position) {
 
-    var geo_text = "緯度:" + position.coords.latitude + "\n";
-    geo_text += "経度:" + position.coords.longitude + "\n";
-    geo_text += "高度:" + position.coords.altitude + "\n";
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    altitude = position.coords.altitude;
+    var geo_text = "緯度:" + latitude + "\n";
+    geo_text += "経度:" + longitude + "\n";
+    geo_text += "高度:" + altitude + "\n";
     geo_text += "位置精度:" + position.coords.accuracy + "\n";
     geo_text += "高度精度:" + position.coords.altitudeAccuracy  + "\n";
     geo_text += "移動方向:" + position.coords.heading + "\n";
@@ -18,7 +22,10 @@ function write(position) {
 
     geo_text += "取得時刻:" + date.toLocaleString() + "\n";
 
+    pos_list.push([latitude, longitude]);
+
     document.getElementById('position_view').innerHTML = geo_text;
+    document.getElementById('position_view').innerHTML = pos_list;
 }
 
 init();
