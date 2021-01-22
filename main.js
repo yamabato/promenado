526e94b77377d1af0d3dd82352b1e2f333c1a79f
yamabato <harutakutsukawa0601@gmail.com>
2021-01-22 21:48:04 +0900

    総移動距離の計算を追加

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 var watch_id;
 var pos_list;
+var distance;
+
+function measure_distance(pos1, pos2) {
+  lat1 = pos1[0];
+  lng1 = pos1[1];
+  lat2 = pos2[0];
+  lng2 = pos2[1];
+  lat1 *= Math.PI / 180;
+  lng1 *= Math.PI / 180;
+  lat2 *= Math.PI / 180;
+  lng2 *= Math.PI / 180;
+  return 6371 * Math.acos(Math.cos(lat1) * Math.cos(lat2) * Math.cos(lng2 - lng1) + Math.sin(lat1) * Math.sin(lat2));
+}
 
 function init() {
     watch_id = navigator.geolocation.watchPosition(write, function(e) { alert(e.message); }, {"enableHighAccuracy": true, "timeout": 20000, "maximumAge": 2000});
@@ -23,9 +36,14 @@ function write(position) {
     geo_text += "取得時刻:" + date.toLocaleString() + "\n";
 
     pos_list.push([latitude, longitude]);
+    len = pos_list.length;
+    if (len >= 2){
+        distance += measure_distance(pos_list[len-2],pos_list[len-1]);
+    }
+
+    geo_text += "総移動距離:" + distance + "\n";
 
     document.getElementById('position_view').innerHTML = geo_text;
-    document.getElementById('position_view').innerHTML = pos_list;
 }
 
 init();
